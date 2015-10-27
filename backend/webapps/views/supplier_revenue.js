ec310690c5e0d6716dc46ae117ce911d9a151746
wang-wr <wangwr@y-car.com.cn>
2015-10-27 14:08:09 +0800

    次卡结算流水

--- backend/webapps/views/supplier_revenue.js
+++ backend/webapps/views/supplier_revenue.js
@@ -24,12 +24,12 @@ define(["views/modules/base"],function(base){
             }
             return "<span class='status status0'>次卡订单</span>";
         },width:80,fillspace:false},
-        {id:"order_cost", header:"订单流水",width:120,format:base.priceFormat,fillspace:false,sort:"string"},
-        {id:"order_reward", header:"订单奖励",width:120,format:base.priceFormat,fillspace:false},
         {id:"customer_name", header:"订单用户",width:120,fillspace:false},
         {id:"car_number", header:"车牌号",width:150,fillspace:false},
-        {id:"reward_note", header:"奖励原因",width:150,fillspace:false},
-        {id:"settled_check",header:"结算状态",template:function(obj,common){
+        //{id:"reward_note", header:"奖励原因",width:150,fillspace:false},
+        {id:"order_cost", header:"订单流水",width:120,format:base.priceFormat,fillspace:false,sort:"string"},
+        {id:"order_reward", header:"订单奖励",width:120,format:base.priceFormat,fillspace:false},
+        {id:"settled_check",header:"结算",template:function(obj,common){
             if(obj.settled){
                 return "<span class='status status1'>已结算</span>";
             }
@@ -95,12 +95,12 @@ define(["views/modules/base"],function(base){
         rows:[
             {
                 id: "price_show",
-                height: 180,
-                template:
-                "<div class='big_strong_text'><span>总流水：￥#total_revenue#</span><span>总奖励：￥#total_reward#</span><span>总金额：￥#total_price#</span></div>"
-                +"<div class='big_strong_text'><span>已结算流水：￥#settled_revenue#</span><span>已结算奖励：￥#settled_reward#</span><span>已结算金额：￥#settled_price#</span></div>"
-                +"<div class='big_strong_text'><span>未结算流水：￥#not_settled_revenue#</span><span>未结算奖励：￥#not_settled_reward#</span><span>未结算金额：￥#not_settled_price#</span></div>"
-                +"<div class='big_strong_text'><span>待结算流水：￥#wait_settled_revenue#</span><span>待结算奖励：￥#wait_settled_reward#</span><span>待结算金额：￥#wait_settled_price#</span></div>",
+                height: 50,
+                template:"<div class='big_strong_text'><span>总流水：￥#total_revenue#</span><span>已结算流水：￥#settled_revenue#</span><span>未结算流水：￥#not_settled_revenue#</span><span>待结算流水：￥#wait_settled_revenue#</span></div>",
+                //"<div class='big_strong_text'><span>总流水：￥#total_revenue#</span><span>总奖励：￥#total_reward#</span><span>总金额：￥#total_price#</span></div>"
+                //+"<div class='big_strong_text'><span>已结算流水：￥#settled_revenue#</span><span>已结算奖励：￥#settled_reward#</span><span>已结算金额：￥#settled_price#</span></div>"
+                //+"<div class='big_strong_text'><span>未结算流水：￥#not_settled_revenue#</span><span>未结算奖励：￥#not_settled_reward#</span><span>未结算金额：￥#not_settled_price#</span></div>"
+                //+"<div class='big_strong_text'><span>待结算流水：￥#wait_settled_revenue#</span><span>待结算奖励：￥#wait_settled_reward#</span><span>待结算金额：￥#wait_settled_price#</span></div>",
                 data: webix.copy(price_template)
             },
             {cols:[
@@ -146,23 +146,23 @@ define(["views/modules/base"],function(base){
             var item = $$("table_list").getItem(row);
             if(item.settled){
                 price.settled_revenue += item.order_cost*1;
-                price.settled_reward += item.order_reward*1;
+                //price.settled_reward += item.order_reward*1;
             }else{
                 price.not_settled_revenue += item.order_cost*1;
-                price.not_settled_reward += item.order_reward*1;
+                //price.not_settled_reward += item.order_reward*1;
             }
             if(item.settled_check){
                 price.wait_settled_revenue += item.order_cost*1;
-                price.wait_settled_reward += item.order_reward*1;
+                //price.wait_settled_reward += item.order_reward*1;
             }
             price.total_revenue += item.order_cost*1;
-            price.total_reward += item.order_reward*1;
+            //price.total_reward += item.order_reward*1;
             //$$("price_show").parse(price);
         });
-        price.total_price = price.total_revenue+price.total_reward;
-        price.settled_price = price.settled_revenue+price.settled_reward;
-        price.not_settled_price = price.not_settled_revenue+price.not_settled_reward;
-        price.wait_settled_price = price.wait_settled_revenue+price.wait_settled_reward;
+        //price.total_price = price.total_revenue+price.total_reward;
+        //price.settled_price = price.settled_revenue+price.settled_reward;
+        //price.not_settled_price = price.not_settled_revenue+price.not_settled_reward;
+        //price.wait_settled_price = price.wait_settled_revenue+price.wait_settled_reward;
         $$("price_show").parse(price);
     }
 
@@ -175,9 +175,13 @@ define(["views/modules/base"],function(base){
             return "";
         }
         var param = {supplier_id:supplier,start:base.format_time(start),end:base.format_time(end)};
-        base.postReq("community/order_rewards.json",param,function(data){
+        base.postReq("community/order_revenue.json",param,function(data){
             $$("table_list").clearAll();
-            $$("table_list").parse(data);
+            for(var a in data){
+                if(data[a].reward_code === 'every_order_percent_reward'){
+                    $$("table_list").add(data[a]);
+                }
+            }
             countPrice();
         })
     };
