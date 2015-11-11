956a1305d950a51b89fff5b0ef37bf006df1723c
wang-wr <wangwr@y-car.com.cn>
2015-11-11 18:54:45 +0800

    修复单品商城的类型字段

--- backend/webapps/views/forms/store_type_form.js
+++ backend/webapps/views/forms/store_type_form.js
@@ -1,6 +1,4 @@
-define(["views/modules/base",
-    "views/modules/upload",
-    "views/forms/store_product"],function(base,upload,store_product){
+define(["views/modules/base"],function(base){
 
     var type_options = [
         {id:"store_home_ware",value:"商城单品"},
@@ -10,8 +8,8 @@ define(["views/modules/base",
 
     var elements = [
         {view:"text",id:"ware_type_id",name:"ware_type_id",hidden:true},
-        {view: "richselect", id:"ware_type_code",name:"ware_type_code",options:type_options,label:"单品分类",placeholder:"请选择单品类别",width:350},
-        {view: "text", label:"单品标题",name:"ware_type_name", placeholder: "请输入单品标题",width:350,value:""},
+        {view: "richselect", id:"ware_type_code",name:"ware_type_code",options:type_options,label:"单品分类",placeholder:"请选择单品类别",required:true,width:350},
+        {view: "text", label:"单品标题",name:"sub_name", placeholder: "请输入单品标题",width:350,value:"",required:true},
     ];
 
     var from_ui = {
@@ -24,15 +22,17 @@ define(["views/modules/base",
     };
 
     var button_ui = {margin:20,cols:[{},{view:"button",label:"确定",width:80,click:function(){
-        var formdata = $$("form_view").getValues();
-        var action = "/v2/api/store/ware_type/create.json";
-        base.postReq(action,formdata,function(){
-            webix.message("数据更新成功");
-            if(typeof(submit_call_back)==='function'){
-                submit_call_back();
-            }
-            webix.$$("pop_win").close();
-        });
+        if($$("form_view").validate()){
+            var formdata = $$("form_view").getValues();
+            var action = "/v2/api/store/ware_type/create.json";
+            base.postReq(action,formdata,function(){
+                webix.message("数据更新成功");
+                if(typeof(submit_call_back)==='function'){
+                    submit_call_back();
+                }
+                webix.$$("pop_win").close();
+            });
+        }
     }},
         {view:"button",label:"取消",width:80,click:function(){
             webix.$$("pop_win").close();
