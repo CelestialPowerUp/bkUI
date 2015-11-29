ed881ea65491378b0869d9c36cedc3f01527026b
zk <liuzq7@gmail.com>
2015-11-29 19:38:33 +0800

    produciton_ui  auto committed by _git.sh

--- distribution_tools/_ui.sh
+++ distribution_tools/_ui.sh
@@ -4,7 +4,8 @@
 #git stash
 #git checkout feature_backend_ui &&
 #git pull github_core  &&
-echo $4 $1:$3
-rsync -avc $4 --rsh="sshpass -p $2 ssh -o StrictHostKeyChecking=no -l root"  $1:$3
- 
+$ssh_shortcut=production
+# rsync -avc $4 --rsh="sshpass -p $2 ssh -o StrictHostKeyChecking=no -l root"  $1:$3
+rsync -avc --progress  $1  $ssh_shortcut:$2 
+
 #git stash pop
