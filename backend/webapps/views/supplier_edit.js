535539feea081dd984bbb6677c12442adee37598
wang-wr <wangwr@y-car.com.cn>
2016-01-13 11:13:39 +0800

    修改页面显示

--- backend/webapps/views/supplier_edit.js
+++ backend/webapps/views/supplier_edit.js
@@ -94,7 +94,7 @@ define(["views/modules/base","views/webix/baidumap"],function(base){
             }}
         },
         {view: "text", label:"首单奖励",name:"first_order_reward",placeholder: "首单奖励金额",width:350,required:true},
-        {view: "text", label:"匹配距离",name:"adaption_distance",placeholder: "社区店匹配距离最距离",width:350,required:false},
+        {view: "text", label:"匹配距离（米）",name:"adaption_distance",placeholder: "社区店最大匹配距离",width:350,required:false},
         {view: "text", label:"银行卡号",id:"bank_card",name:"bank_card",placeholder:"这里面填写银行卡号", width:350},
 
         {
