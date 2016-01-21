d6a2ca7f693c68c93270704ce14532173602e76e
WangShuai <mkwork@qq.com>
2016-01-21 21:26:29 +0800

    task#146,添加推送信息表单结构

--- backend/webapps/views/push_info.js
+++ backend/webapps/views/push_info.js
@@ -1,17 +1,12 @@
-define(["models/order",
-        "views/forms/order_product",
+define([
         "views/modules/base",
-        "views/forms/order_product",
-        "views/forms/user_car_model",
-        "views/order_details",
-		"views/forms/supplier",
-		"views/webix/baidumap"], function(order,product,base,order_product,user_car_model,order_details,supplier){
+		"views/windows/store_wares_win",], function(base,store_wares_win){
 
-
+	/*控制可选版本选择按钮*/
 	var button_ui = {cols:[
 		{},{},{},
-		{view:"button",label:"选中全部",width:80,click:function(){
-			var datas = $$("role_api_list").serialize();
+		{view:"button",label:"全选",width:80,click:function(){
+			var datas = $$("app_list").serialize();
 			for(var i=0;i<datas.length;i++){
 				try{
 					datas[i].$check = true;
@@ -19,10 +14,10 @@ define(["models/order",
 					console.log(datas[i]);
 				}
 			}
-			$$("role_api_list").refresh();
+			$$("app_list").refresh();
 		}},
-		{view:"button",label:"取消全部",width:80,click:function(){
-			var datas = $$("role_api_list").serialize();
+		{view:"button",label:"全不选",width:80,click:function(){
+			var datas = $$("app_list").serialize();
 			for(var i=0;i<datas.length;i++){
 				try{
 					datas[i].$check = false;
@@ -30,10 +25,10 @@ define(["models/order",
 					console.log(datas[i]);
 				}
 			}
-			$$("role_api_list").refresh();
+			$$("app_list").refresh();
 		}},
 		{view:"button",label:"反选",width:80,click:function(){
-			var datas = $$("role_api_list").serialize();
+			var datas = $$("app_list").serialize();
 			for(var i=0;i<datas.length;i++){
 				try{
 					datas[i].$check = !(datas[i].$check);
@@ -41,10 +36,49 @@ define(["models/order",
 					console.log(datas[i]);
 				}
 			}
-			$$("role_api_list").refresh();
-		}},
+			$$("app_list").refresh();
+		}}
 	]};
 
+	/*控制推送用户选择按钮*/
+	var chosen_users_check_button_ui = {cols:[
+		{},{},{},
+		{view:"button",label:"全选",width:80,click:function(){
+			var datas = $$("user_list").serialize();
+			for(var i=0;i<datas.length;i++){
+				try{
+					datas[i].$check = true;
+				}catch(e){
+					console.log(datas[i]);
+				}
+			}
+			$$("user_list").refresh();
+		}},
+		{view:"button",label:"全不选",width:80,click:function(){
+			var datas = $$("user_list").serialize();
+			for(var i=0;i<datas.length;i++){
+				try{
+					datas[i].$check = false;
+				}catch(e){
+					console.log(datas[i]);
+				}
+			}
+			$$("user_list").refresh();
+		}},
+		{view:"button",label:"反选",width:80,click:function(){
+			var datas = $$("user_list").serialize();
+			for(var i=0;i<datas.length;i++){
+				try{
+					datas[i].$check = !(datas[i].$check);
+				}catch(e){
+					console.log(datas[i]);
+				}
+			}
+			$$("user_list").refresh();
+		}}
+	]};
+
+	/*控制车品牌选择按钮*/
 	var car_brand_check_button_ui = {cols:[
 		{},{},{},
 		{view:"button",label:"全选",width:80,click:function(){
@@ -61,13 +95,15 @@ define(["models/order",
 		}},
 	]};
 
+	/*搜索用户*/
 	var search_user_button_ui = {cols:[
 		{},{},{},
-		{view:"button",label:"根所选品牌筛选用户",width:200,click:function(){
+		{view:"button",label:"根据所选品牌筛选用户",click:function(){
+			$$("user_list").clearAll();
 			var selectedCarBrandIds = $$("car_brands").getSelectedId();
 			console.log(selectedCarBrandIds==='');
 			if(!selectedCarBrandIds){
-				webix.message({ type:"error",expire:5000,text:"筛用户前，请先选车品牌。"});
+				webix.message({ type:"error",expire:5000,text:"想选用户？好歹选点车吧。" });
 				return;
 			}
 
@@ -80,21 +116,69 @@ define(["models/order",
 				carBrandIds.push(selectedCarBrandIds);
 			}
 
-			//获取符合条件用户信息 TODO
-			var url_get_users = "";
-			base.getReq("/v1/api/app_packages/enable_client.json",function(data){
+			//获取符合条件用户信息
+			var url_get_users = "/v1/api/push_to_be_chosen_users.json";
+			var param = carBrandIds;
+			base.postReq("/v1/api/push_to_be_chosen_users.json",param,function(data){
+				var chosen_users = [];
+				if(data && data instanceof Array){
+
+					/*获取已选择版本*/
+					var selectedAppVersions = [];
+					var datas = $$("app_list").serialize();
+					for(var i=0;i<datas.length;i++){
+						try{
+							if(datas[i].$check===true){
+								var app_info = datas[i].os_type+" "+datas[i].app_version;
+								selectedAppVersions.push(app_info);
+							}
+						}catch(e){
+							console.log(datas[i]);
+						}
+					}
+					if(selectedAppVersions.length<1){
+						webix.message({type:"error",expire:5000,text:"亲，建议您先至少选一个版本，否则推给谁？"});
+					}
+
+					for(var i=0;i<data.length;i++){
+						var app_version_array = data[i].app_versions.split(",");
+						if(  array_has_intersection(selectedAppVersions,app_version_array)){
+							data[i].$check=true;
+							chosen_users.push(data[i]);
+						}
+					}
+				}
+				$$("user_list").parse(chosen_users);
 			},function(data){
 			});
-
-
-			var data = [
-				{"$check":true,"user_id":1,"phone_number":"18210237734","user_name":"王帅","app_versions":"android 5.0.0"}
-			];
-			$$("user_list").parse(data);
-
-			console.log(carBrandIds);
 		}}
 	]};
+
+	/*判断两数组是否有交集*/
+	var array_has_intersection = function(array1,array2){
+		if(!array1 || !array2){
+			return false;
+		}
+		if( (array1 instanceof Array) && (array2 instanceof Array) ){
+			for(var i=0;i<array1.length;i++){
+				var tmp = array1[i];
+				for(var j=0;j<array2.length;j++){
+					if(tmp == array2[j] ){
+						return true;
+						break;
+					}
+				}
+			}
+		}else{
+			return false;
+		}
+	};
+
+	/*选择单品后回显数据*/
+	store_wares_win.$add_callback(function(ware){
+		$$("store_product").setValue(ware.ware_name+"("+ware.product_name+")");
+		$$("push_open_target_params").setValue(ware.ware_id);
+	});
 
 	var form_ui = {
 		view: "form",
@@ -117,10 +201,44 @@ define(["models/order",
 			{ view:"datepicker", timepicker:true, label:"推送时间：", name:"plan_push_time", stringResult:true, format:"%Y-%m-%d %H:%i" ,width:350 },
 			{ view:"textarea",label:"推送内容:", name:"push_content",id:"push_content",height:150,required:true,placeholder:"推送内容",value:"" },
 			{ view:"text", id:"push_open_target_description",name:"push_open_target_description",label:"划开描述:",required:true,placeholder:"划开内容简介",value:""},
-			{ view:"text", id:"push_open_target_params",name:"push_open_target_params",label:"划开描述:",required:true,placeholder:"",value:"",hidden:true},
-			{ view:"richselect", id:"push_open_target_type",name:"push_open_target_type",options:[{"id":"h5",value:"H5页面"},{"id":"store_product",value:"单品"}],label:"划开目标:",required:true,placeholder:"划开目标",value:"H5页面"},
-			{ view:"text", id:"h5_url",name:"h5_url",label:"H5地址:",required:true,placeholder:"H5地址",value:""},
+			{ view:"text", id:"push_open_target_params",name:"push_open_target_params",label:"划开参数:",required:true,placeholder:"",readonly:true,disabled:true,value:"",hidden:false},
+			{ view:"richselect", id:"push_open_target_type",name:"push_open_target_type",
+				options:[
+					{"id":"h5",value:"H5页面"},
+					{"id":"store_product",value:"单品"}
+				],
+				label:"划开目标:",required:true,placeholder:"划开目标",value:"h5",
+				on:{
+					onChange:function(newv, oldv){
+						console.log(newv,oldv);
 
+						$$("push_open_target_params").setValue('');
+						if(newv=='store_product'){
+							$$("h5_url").setValue('');
+							$$("h5_url").hide();
+
+							$$("store_product").show();
+						}
+						if(newv=='h5'){
+							$$("store_product").setValue('');
+							$$("store_product").hide();
+
+							$$("h5_url").show();
+						}
+					}
+				}
+			},
+			{ view:"text", id:"h5_url",name:"h5_url",label:"H5地址:",required:true,placeholder:"H5地址",value:"",keyPressTimeout:100,on:{
+				"onTimedKeyPress":function(obj){
+					console.log(obj);
+					$$("push_open_target_params").setValue($$("h5_url").getValue());
+
+				}
+			}},
+			{view: "text", label:"单品",id:"store_product", name:"store_product",placeholder: "单击选择可选单品",readonly:true,hidden:true,click:function(){
+				//单品
+				webix.ui(store_wares_win.$ui).show();
+			}},
 			//版本
 			{
 				view:"toolbar",css: "highlighted_header header5",height:40,width:820,
@@ -131,7 +249,7 @@ define(["models/order",
 			{
 				view: "list",
 				css: "tasks_list",
-				id:"role_api_list",
+				id:"app_list",
 				height:300,
 				width:650,
 				type: {
@@ -165,7 +283,7 @@ define(["models/order",
 				view:"dataview",
 				id:"car_brands",
 				select:"multiselect",
-				height:300,
+				height:200,
 				width:50,
 				scroll:true,
 				template:"<div class='webix_strong'>#name#</div>" ,
@@ -188,7 +306,7 @@ define(["models/order",
 				view:"toolbar",css: "highlighted_header header5",height:40,width:820,
 				elements:[
 					{view:"label", align:"left",label:"用户列表",height:30},
-					car_brand_check_button_ui
+					chosen_users_check_button_ui
 				],hidden:false},
 			{
 				view: "list",
@@ -206,7 +324,7 @@ define(["models/order",
 						return "<span class='"+(obj.$check?"":"")+"'>"+type.check(obj)+"<span class='list_text view_list_item'>"+obj.user_name+"</span>"
 								+"<span class='view_list_item'>"+obj.phone_number+"</span>"
 								+"<span class='view_list_item'>"+obj.app_versions+"</span>";
-					} //{"$check":true,"user_id":1,"phone_number":"18210237734","user_name":"王帅","app_versions":"android 5.0.0"}
+					}
 				},
 				data: [],
 				on: {
@@ -323,7 +441,7 @@ define(["models/order",
 				for(var i=0;i<all.length;i++){
 					data.push(parse_check_data(all[i],checked));
 				}
-				$$("role_api_list").parse(data);
+				$$("app_list").parse(data);
 			});
 		});
 
