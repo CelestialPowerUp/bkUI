d3f4bbfdd9c64ab0613eba949953b07d1fd95481
wang-wr <wang-wr@y-car.com.cn>
2016-02-26 19:25:33 +0800

    task#249,文章内容管理

---
+++ backend/webapps/views/article_list.js
@@ -0,0 +1,107 @@
+/**
+ * Created by wangwr on 2015/11/6.
+ */
+
+define(["views/modules/base",
+        "views/menus/popup_menu",
+        "views/windows/article_edit_win"],function(base,menu,form){
+
+    var img_fomat = function(obj){
+        var img = obj.title_img_view;
+        return '<img src="'+img.thumbnail_url+'" class="content" ondragstart="return false"/>';
+    };
+
+    var img_list_ui = {
+        view: "dataview",
+        id: "img_view",
+        css: "nav_list",
+        select: true,
+        scroll: true,
+        drag:true,
+        type: {width: 350, height: 260},
+        template: img_fomat,
+        on:{"onItemClick":function(id, e, node){
+            var item = this.getItem(id);
+            if(item.link_href==='#add'){
+                webix.ui(form.$ui).show();
+            }else{
+                $$("pp_menu").show(e);
+            }
+        }}
+    }
+
+    var toolbar_ui = {view:"toolbar",css: "highlighted_header header5",height:45, elements:[
+        {view:"label", align:"left",label:"推广列表",height:30},
+        {view: "richselect",id:"generalize_code",label:"类型:",labelWidth:60,value:"banners",options:[
+            {id:"banners",value:"头部"},
+            {id:"good_suggest",value:"精品推荐"}
+        ],placeholder:"推广类型",width:250},
+        {view:"segmented",id:"status_type", multiview:true,width:150, value:"enabled", options:[
+            { id:"enabled", value:"启用" }, // the initially selected segment
+            { id:"disabled", value:"停用" }]
+        },
+        { view: "button", type: "iconButton", icon: "sort-alpha-asc", label: "排序提交", width: 120, click: function(){
+            //todo
+            var list = $$("img_view").serialize();
+            for(var a in list){
+                if(list[a].link_href === '#add'){
+                    list.splice(a,1);
+                    break;
+                }
+            }
+            base.postReq("generalize/sort.json",list,function(){
+                base.$msg.info("排序更新成功");
+            });
+
+        }}
+    ]};
+
+    var layout = {
+        rows:[
+            toolbar_ui,img_list_ui
+        ]
+    };
+
+    var refresh = function(){
+        base.getReq("/v2/api/home/"+$$("generalize_code").getValue()+"/"+$$("status_type").getValue(),function(data){
+            $$("img_view").clearAll();
+            for(var a in data){
+                $$("img_view").add(data[a]);
+            }
+            $$("img_view").add( {
+                "link_href": "#add",
+                "title_img_view": {
+                    "thumbnail_url": "http://7xiqe8.com2.z0.glb.qiniucdn.com/QQ截图20151106142133.jpg"
+                }
+            });
+        });
+    };
+
+    var menus = [
+        {value:"edit",label:"编辑",click:function(){
+            webix.ui(form.$ui).show();
+            var item = $$("img_view").getSelectedItem();
+            form.$init_data(item);
+        }}
+    ];
+
+    return{
+        $ui:layout,
+        $oninit:function(app,scope){
+            webix.$$("title").parse({title: "商城管理", details: "推广信息"});
+            setTimeout(function(){
+                menu.$add_menus(menus);
+            },500);
+            form.$addCallBack(function(){
+                refresh();
+            });
+            $$("generalize_code").attachEvent("onChange", function(newv, oldv){
+                refresh();
+            });
+            $$("status_type").attachEvent("onChange", function(newv, oldv){
+                refresh();
+            });
+            refresh();
+        }
+    }
+});
---
+++ backend/webapps/views/windows/article_edit_win.js
@@ -0,0 +1,119 @@
+define(["views/modules/base",
+    "views/modules/upload",
+    "views/windows/store_wares_win",
+    "views/windows/topic_list_win"],function(base,upload,store_wares_win,topic_list_win){
+
+    store_wares_win.$add_callback(function(ware){
+        $$("link_text").setValue(ware.ware_name+"("+ware.product_name+")");
+        $$("link_id").setValue(ware.ware_id);
+    });
+
+    topic_list_win.$add_callback(function(topic){
+        $$("link_text").setValue(topic.topic_name);
+        $$("link_id").setValue(topic.topic_id);
+    });
+
+    var clear_link_info = function(){
+        $$("link_text").setValue('');
+        $$("link_id").setValue('');
+    };
+
+    var img_fomat = function(obj){
+        if(typeof obj.thumbnail_url === 'undefined' || obj.thumbnail_url === ""){
+            return '<img src="http://7xiqd8.com2.z0.glb.qiniucdn.com/Fg_yYLTcb6lsCJaKI9DMIBeD53VF" class="content" ondragstart="return false"/>';
+        }
+        return '<img onclick="window.open(\''+obj.raw_url+'\')" src="'+obj.thumbnail_url+'" class="content" ondragstart="return false"/>';
+    };
+
+    var elements = [
+        {view:"text",id:"article_id",name:"article_id",hidden:true},
+        {view:"text",name:"index_no",hidden:true},
+        {view:"text",id:"article_pic_id",name:"article_pic_id",hidden:true},
+        {view:"text",label:"标题",id:"article_title",name:"article_title",required:true},
+        {height:150,width:400,id:"cover_img",template:img_fomat},
+        {height:25,template:"<a id='cover_img_pickfiles' style='padding-left: 150px' href='javascript:;'>[上传图片]</a>",
+            on:{"onAfterRender":function(){
+                upload.$bind_upload("cover_img_pickfiles",function(data){
+                    if(data.code==='00000'&&data.data!=null){
+                        var item = data.data;
+                        item.img_id = item.id;
+                        $$("cover_img").parse(item);
+                        $$("article_pic_id").setValue(item.img_id);
+                    }
+                });
+            }}},
+        { view:"radio", name:"status", label:"状态",value:"disabled", width:350,required:true,options:[
+            { value:"启用", id:'enabled' },
+            { value:"停用", id:'disabled' },
+            { value:"发布", id:'published' },
+            { value:"隐藏", id:'unpublished' }
+        ]},
+        {view:"text",label:"链接",name:"article_href",placeholder: "http://",required:true}
+    ];
+
+    var from_ui = {
+        id:"form_view",
+        view:"form",
+        elementsConfig:{
+            labelWidth: 50,
+            labelPosition:"left"
+        },
+        elements:elements
+    };
+
+    var button_ui = {margin:20,cols:[{},
+        {view:"button",label:"确定",width:80,click:function(){
+            if (!$$("form_view").validate()){
+                base.$msg.error("请输入正确的参数");
+                return;
+            }
+            var formdata = $$("form_view").getValues();
+            var action = "generalize/update.json";
+            if(formdata.generalize_id.length===0){
+                action = "generalize/create.json";
+            }
+            console.log(formdata);
+            base.postReq(action,formdata,function(data){
+                base.$msg.info("数据提交成功");
+                if(typeof callBack === 'function'){
+                    callBack(data);
+                }
+                webix.$$("pop_win").close();
+            });
+        }},
+        {view:"button",label:"取消",width:80,click:function(){
+            webix.$$("pop_win").close();
+        }}]
+    };
+
+    var init_data = function(obj){
+        if(obj){
+            $$("form_view").parse(obj);
+            $$("cover_img").parse(obj.title_img_view);
+        }
+    };
+
+    var win_ui = {
+        view:"window",
+        modal:true,
+        id:"pop_win",
+        position:"center",
+        head:"文章信息编辑",
+        body: {
+            type:"space",
+            rows:[from_ui,button_ui]
+        }
+    };
+
+    var callBack = null;
+
+    return {
+        $ui:win_ui,
+        $init_data:init_data,
+        $addCallBack:function(func){
+            if(typeof func === 'function'){
+                callBack = func;
+            }
+        }
+    };
+});
